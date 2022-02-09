593a22fd4c48dae800e5926957dcc80104206864
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-08 16:49:20 -0800

    Create Square changes color from white to black.py
    changes the color of the square from white to black

---
+++ Square changes color from white to black.py
@@ -0,0 +1,29 @@
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode([400, 400])
+
+red = 0
+green = 0
+blue = 0
+
+timer = 255
+while timer >= 0:
+
+    window.fill((128, 128, 128))
+
+    # Make a color that starts white and becomes black
+
+    color = (red, green, blue)
+    
+    # Draw the rectangle of that color
+    rectangle = pygame.Rect(100, 100, 200, 200)
+    pygame.draw.rect(window, color, rectangle)
+
+    red += 1
+    green += 1
+    blue += 1
+
+    pygame.display.flip()
+    pygame.time.wait(10)
+    timer -= 1
