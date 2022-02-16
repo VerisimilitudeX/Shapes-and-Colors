9bae45780de9e27f67fcb78cc9216fa60883256a
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-16 11:59:45 -0800

    Update Circle from user input.py

--- Circle from user input.py
+++ Circle from user input.py
@@ -4,13 +4,11 @@ pygame.init()
 window = pygame.display.set_mode([400, 400])
 window.fill((0, 0, 0))
 
-# Get colors from user input
 red = int(input("How much red do you want in the circle? "))
 green = int(input("How much red do you want in the circle? "))
 blue = int(input("How much red do you want in the circle? "))
 color = (red, green, blue)
 
-# Draw circle
 pygame.draw.circle(window, color, (200, 200), 100)
 
 pygame.display.flip()
