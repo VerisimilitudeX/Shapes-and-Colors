d7415d1b126e0d9ce952db58743b4a3c7df0bd82
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-16 11:59:33 -0800

    Update Car with relative position.py

--- Car with relative position.py
+++ Car with relative position.py
@@ -8,16 +8,12 @@ car_color = (40, 20, 120)
 window = pygame.display.set_mode([400, 400])
 window.fill((200, 200, 255))
 
-# Draw the back wheels
 pygame.draw.circle(window, back_wheel_color, (75, 300), 50)
 pygame.draw.circle(window, back_wheel_color, (325, 300), 50)
 
-# Draw the car
 rectangle = pygame.Rect(50, 100, 300, 200)
 pygame.draw.rect(window, car_color, rectangle)
 
-
-# Draw the front wheels
 pygame.draw.circle(window, front_wheel_color, (125, 300), 50)
 pygame.draw.circle(window, front_wheel_color, (350, 300), 50)
 
