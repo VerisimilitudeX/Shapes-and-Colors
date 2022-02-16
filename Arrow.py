8e555e640f3c4cb1b09db9a0e04f1b905aa42c99
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-16 11:58:56 -0800

    Update Arrow.py

--- Arrow.py
+++ Arrow.py
@@ -5,7 +5,6 @@ window = pygame.display.set_mode([400, 400])
 window.fill((200, 200, 255))
 color = (0, 30, 80)
 
-# Declare the points
 top_left = (0, 150)
 bottom_left = (0, 250)
 top_top = (300, 100)
@@ -14,11 +13,7 @@ bottom_top = (300, 250)
 bottom_bottom = (300, 300)
 middle_center = (400, 200)
 
-
-# Draw the arrow
 pygame.draw.polygon(window, color, [top_left, bottom_left, bottom_top, bottom_bottom, middle_center, top_top, top_bottom])
-
-
 
 pygame.display.flip()
 input("Press enter to end the program")
