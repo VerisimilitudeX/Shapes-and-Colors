36f815a3c63c333e5d63ab44e53a65221efa06b3
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-17 12:40:40 -0800

    Update Letter T.py

--- Letter T.py
+++ Letter T.py
@@ -5,7 +5,6 @@ window = pygame.display.set_mode([300, 300])
 window.fill((0, 0, 0))
 color = (140, 198, 62)
 
-# Declare the points
 top_left = (100, 0)
 top_right = (250, 0)
 top_bottom_left = (100, 50)
@@ -15,7 +14,6 @@ top_bottom_middle_right = (200, 50)
 bottom_left = (150, 350)
 bottom_right = (200, 350)
 
-# Draw the T
 pygame.draw.polygon(window, color, [top_left, top_right, top_bottom_right, top_bottom_middle_right, bottom_right, bottom_left, top_bottom_middle_left, top_bottom_left, top_left])
 
 pygame.display.flip()
