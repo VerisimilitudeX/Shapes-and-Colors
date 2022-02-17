b7802f5b1a5447791b76599bea11a41ee13f8be5
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-17 12:44:59 -0800

    Update Red and Blue Circle.py

--- Red and Blue Circle.py
+++ Red and Blue Circle.py
@@ -4,7 +4,6 @@ pygame.init()
 win = pygame.display.set_mode([300, 300])
 win.fill((255, 255, 255))
 
-# Draw circles here
 pygame.draw.circle(win, (200, 0, 0), (150, 150), 100)
 pygame.draw.circle(win, (0, 0, 200), (150, 150), 50)
 
