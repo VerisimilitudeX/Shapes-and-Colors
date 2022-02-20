96600790d80f1abcd242a3f06a9775649800242f
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:38:21 -0800

    Update Great Gradient.py

--- Great Gradient.py
+++ Great Gradient.py
@@ -1,29 +1,20 @@
 import pygame
 
 pygame.init()
-
 window = pygame.display.set_mode([510, 300])
 
 top = 0
-
 left = 0
-
 r = 0
-
 g = 0
-
 b = 0
 
 while left < 510:
-
     stripe = pygame.Rect(left, top, 2, 300)
-
     pygame.draw.rect(window, (r, g, b), stripe)
 
     r += 1
-
     b += 1
-
     left += 2
 
     pygame.display.flip()
