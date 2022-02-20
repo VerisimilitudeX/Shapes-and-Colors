720f12cba082f0b4bd1e0cb38d6795ac4189e0c6
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:38:56 -0800

    Update Hue Master.py

--- Hue Master.py
+++ Hue Master.py
@@ -1,33 +1,24 @@
 import random
-
 import pygame
 
 pygame.init()
-
 window = pygame.display.set_mode([200, 200])
 
 rand_r = random.randint(0, 255)
-
 rand_g = random.randint(0, 255)
-
 rand_b = random.randint(0, 255)
 
 rand_color = (rand_r, rand_g, rand_b)
-
 rand_rect = pygame.Rect(0, 0, 200, 200)
 
 pygame.draw.rect(window, rand_color, rand_rect)
-
 pygame.display.flip()
 
 guess_r = int(input("What is the r value? "))
-
 guess_g = int(input("What is the g value? "))
-
 guess_b = int(input("What is the b value? "))
 
 guess_color = (guess_r, guess_g, guess_b)
-
 window = pygame.display.set_mode([400, 200])
 
 window.fill(guess_color)
@@ -37,7 +28,6 @@ pygame.draw.rect(window, rand_color, rand_rect)
 pygame.display.flip()
 
 print("Your guess color was " + str(guess_color) + " . ")
-
 print("The actual color was " + str(rand_color) + " . ")
 
 input("Press enter to end the program")
